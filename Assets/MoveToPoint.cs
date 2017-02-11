286c44ae350b66160e5118a81c83a797a9ee301f
Nathaniel Scott Stevens <nathaniel.scott.stevens@gmail.com>
2017-02-11 11:16:52 -0500

    Initial Commit.

---
+++ Assets/MoveToPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPoint : MonoBehaviour {
+
+    public Transform player;
+    public Transform[] positions;
+    public float touchCooldown = 1;
+
+    int index = 0;
+    public float lastTouch = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Time.time - lastTouch < touchCooldown)
+        {
+            return;
+        }
+
+		if (Input.touchCount > 0)
+        {
+            player.position = positions[index].position;
+            player.rotation = positions[index].rotation;
+
+            lastTouch = Time.time;
+            index++;
+            if (index >= positions.Length)
+            {
+                index = 0;
+            }
+        }
+	}
+}
